f5f534b996541a940d0930ae5b28cc79ef5386b7
gunelaliyeva <gunel.aliyeva.3011@gmail.com>
2021-05-05 10:58:00 +0400

    api changed

--- api_basic/views.py
+++ api_basic/views.py
@@ -42,9 +42,7 @@ def voice_to_text(request):
         decoded_data = base64.decodebytes(audio_bytes)
         new = base64.b64decode(audio_bytes)
         audio_file = sr.AudioFile(decoded_data)
-
-        data = b''.join(decoded_data)
-        ad = sr.AudioData(data, 16000, 2)
+        ad = sr.AudioData(decoded_data, 16000, 2)
         # with audio_file as source:
         #     audio = r.record(source, duration=5)
         return JsonResponse(r.recognize_google(ad), status=200, safe=False)
