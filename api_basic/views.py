e83f1629e3cbf0079d58ac1531307b9b60c3824e
gunelaliyeva <gunel.aliyeva.3011@gmail.com>
2021-05-05 12:57:01 +0400

    api changed

--- api_basic/views.py
+++ api_basic/views.py
@@ -30,7 +30,7 @@ def article_list(request):
 def voice_to_text(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        print(data["data"])
+        # print(data["data"])
 
         r = sr.Recognizer()
         r.energy_threshold = 300
@@ -39,13 +39,14 @@ def voice_to_text(request):
 
         # audio = base64.decodestring(data["data"])
         audio_bytes = data["data"].encode('utf-8')
-        print(audio_bytes)
+        # print(audio_bytes)
         # print(audio_bytes)
         decoded_data = base64.decodebytes(audio_bytes)
         new = base64.b64decode(audio_bytes)
 
         output_file = open('Output.wav', 'w', encoding="utf-8")
         output_file.write(audio_bytes.decode("utf-8"))
+        print(audio_bytes.decode("utf-8"))
         output_file.close()
 
 
