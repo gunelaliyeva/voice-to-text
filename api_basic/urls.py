0bf2ddb0d5c449a06c08d6af0a93ca460043d9f6
gunelaliyeva <gunel.aliyeva.3011@gmail.com>
2021-05-07 12:55:41 +0400

    api changed

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import article_list, voice_to_text
+from .views import voice_to_text
 
 urlpatterns = [
-    path('article/', article_list),
     path('voiceToText/', voice_to_text)
 ]
